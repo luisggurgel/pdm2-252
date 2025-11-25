17b66e9a39662d74660c8adff2adec77e2eb8779
luis <luisguilhermegurgelamaral@gmail.com>
2025-11-25 11:52:08 -0300

    commit da atividade 7

---
+++ taveira/atividades/atividade_7/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23lipuzRCHW3RiCu99';
+const githubClientSecret = '4152de2b1417550acb0f646ef7923ee4b4bc4e9e';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
